14b39e989f7f0cf819020852db528e135c2dc4cc
carlos-delgado23 <carlos.delgado.dev@gmail.com>
2020-02-10 14:35:41 -0600

    read and write needs work

--- src/lateNights/Contact.java
+++ src/lateNights/Contact.java
@@ -2,27 +2,27 @@ package lateNights;
 
 public class Contact extends Contacts {
     private String contactName;
-    private long phoneNumber;
+    private String phoneNumber;
     private String email;
-    private boolean availability;
+    private String snapChat;
 
 //    DEFAULT CONSTRUCTOR
     public Contact(){}
 //    CONSTRUCTOR FOR CONTACT
-    public Contact(String name, long phoneNumber, String email, boolean availability){
+    public Contact(String name, String phoneNumber, String email, String snapChat){
         this.contactName = name;
         this.phoneNumber = phoneNumber;
         this.email = email;
-        this.availability = availability;
+        this.snapChat = snapChat;
     }
 
 //    GETTERS
-    public String getContactName() { return contactName; }
-    public String getEmail() { return email; }
-    public long getPhoneNumber() { return phoneNumber; }
+    public String getContactName() { return this.contactName; }
+    public String getEmail() { return this.email; }
+    public String getPhoneNumber() { return this.phoneNumber; }
 //    SETTERS
     public void setContactName(String contactName) { this.contactName = contactName; }
-    public void setPhoneNumber(long phoneNumber) { this.phoneNumber = phoneNumber; }
+    public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
     public void setEmail(String email) { this.email = email; }
-    public void setAvailability(boolean availability) { this.availability = availability; }
+    public void setSnapChat(String snapChat) { this.snapChat = snapChat; }
 }
--- src/lateNights/Contacts.java
+++ src/lateNights/Contacts.java
@@ -1,22 +1,25 @@
 package lateNights;
 
+import jdk.jshell.spi.ExecutionControl;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Scanner;
+import java.nio.file.StandardOpenOption;
+import java.util.*;
 
 public class Contacts {
+
+    public static List<Contact> contactBook = new ArrayList<>();
 
     public static void main(String[] args) {
 
         contactsFile();
-
+        writeContact();
     }
 
 
@@ -43,6 +46,8 @@ public class Contacts {
         }
     }
 
+
+
     static void readContact(){
         try {
             File myCont = new File ("contacts.txt");
@@ -59,9 +64,41 @@ public class Contacts {
 
     }
 
-    static void writeContact(){
-//        List<String> contact = new ArrayList<>();
-        HashMap<String, Contact> contacts = new HashMap<>();
+
+
+    public static void writeContact(){
+//        HashMap<String, Contact> contactBook = new HashMap<>();
+        Contact newContact = new Contact();
+        Scanner scanner = new Scanner(System.in);
+
+    //======CONTACT INFO======//
+        //   SETS NAME
+        System.out.println("New contacts Name: ");
+        String contName = scanner.nextLine();
+        newContact.setContactName(contName);
+        //  SETS PHONE NUMBER
+        System.out.println(contName + "'s phone number?");
+        String phoneNumber = scanner.nextLine();
+        newContact.setPhoneNumber(phoneNumber);
+        //  SETS EMAIL
+        System.out.println("What about "+ contName +"'s email?");
+        String email = scanner.nextLine();
+        newContact.setEmail(email);
+        //  SETS SNAPCHAT
+        System.out.println("How about that SnapChat?");
+        String snapChat = scanner.nextLine();
+        newContact.setSnapChat(snapChat);
+
+        contactBook.add(newContact);
+        try {
+            Path contactFile = Paths.get("Contacts Book", "contacts.txt");
+            Files.writeString(contactFile, contactBook.get(0).getContactName());
+        } catch (IOException ioe){
+            ioe.printStackTrace();
+        }
+
+
+
 
     }
 
